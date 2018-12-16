cdf23fcafbd523c925b6aef8b0dc59e057b83a12
Carlo DiCelico <carlo@minnow.me>
2018-12-16 16:33:32 -0500

    use IOC register for the read/write check

--- src/libpca9606.c
+++ src/libpca9606.c
@@ -11,8 +11,8 @@ pca9606_s configure_handle(pca9606_s driver){
         driver.command = "rw_check";
 
         // Quick & dirty read/write checks; only verifies the interface, should break on the caller's side.
-        i2c_bus_read(&driver, 0x00);
-        i2c_bus_write(&driver, 0x00, driver.data);
+        i2c_bus_read(&driver, 0x18);
+        i2c_bus_write(&driver, 0x18, driver.data);
     }
 
     driver.command = driver.command == NULL ? "init" : driver.command;
